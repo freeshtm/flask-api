58370a358038a6bd3ed0e3d365af20e5fc0f9bb8
freeshtm <tomaszmatusek06@gmail.com>
2025-10-22 08:45:32 +0200

    remove unique requirement from student_id

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ class UserModel(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(80), nullable=False)
-    student_id = db.Column(db.String(80), unique=True, nullable=True)
+    student_id = db.Column(db.String(80), nullable=True)
     university = db.Column(db.String(80), nullable=True)
     average_rating = db.Column(db.Float, default=0.0)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
