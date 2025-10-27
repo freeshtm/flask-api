8cd50eb5c28cefcc9a2ee4ead3533621d124eaad
freeshtm <tomaszmatusek06@gmail.com>
2025-10-27 20:42:51 +0100

    add CORS configuration to app_setup.py

--- app_setup.py
+++ app_setup.py
@@ -1,8 +1,17 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_cors import CORS
 
 app = Flask(__name__) 
+app.url_map.strict_slashes = False
+CORS(app, resources={
+    r"/api/*": {
+        "origins": ["http://localhost:5173", "http://localhost:5174"],
+        "methods": ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        "allow_headers": ["Content-Type", "Authorization"]
+    }
+})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app) 
 api = Api(app)
