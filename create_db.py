ab63b0fcf301e26f7e0d2c8ba4a424e9e85b0b76
freeshtm <tomaszmatusek06@gmail.com>
2025-10-27 21:01:11 +0100

    update import statements

--- create_db.py
+++ create_db.py
@@ -1,4 +1,6 @@
-from api import app, db 
+from app_setup import app, db 
+from models import UserModel, RideModel, RideParticipantModel, RatingModel
 
 with app.app_context():
     db.create_all()
+    print("Database tables created successfully!")
